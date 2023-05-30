fe4044be4ed909cb3d3c4ff20ec4cb03d3efc7e9
m3llad0 <a01655451@tec.mx>
2023-05-29 23:37:29 -0600

    updated checkIsAgent method

--- src/middlewares/permission.ts
+++ src/middlewares/permission.ts
@@ -1,5 +1,7 @@
 import { Response, Request, NextFunction } from 'express';
-import Manager from '../models/';
+import jwt from 'jsonwebtoken';
+import jwkToPem from 'jwk-to-pem';
+import db from '../models';
 
 export default class PermissionMiddleware {
   // Singleton
@@ -16,27 +18,40 @@ export default class PermissionMiddleware {
    * Verify that the current user is an Agent
    */
   public async checkIsAgent(req: Request, res: Response, next: NextFunction): Promise<void> {
-    try {
-      const userId = req.user; // Assuming you have a middleware that sets the user object on the request
-
-      // Fetch the user from the database
-      const user = await Manager.findByPk(userId);
-
-      if (!user) {
-        res.status(401).send({ 'message': 'User not found' });
-        return;
+    if(req.headers.authorization){
+      try {
+        const token = req.headers.authorization.replace('Bearer ', '');
+        const decodedJWT:any = jwt.decode(token, { complete: true });
+        if (!decodedJWT) {
+          res.status(401).send({ code: 'InvalidTokenException', message: 'The token is no valid' });
+        }
+        const userId = decodedJWT.payload.username;
+        console.log(userId);
+        // Fetch the user from the database
+        const user = await db.Manager.findOne({
+          where: {
+            awsCognitoId: {userId}
+          }
+        });
+  
+        if (!user) {
+          res.status(401).send({ 'message': 'User not found' });
+          return;
+        }
+  
+        if (user.role !== 'AGENT') {
+          res.status(403).send({ 'message': 'User is not an agent' });
+          return;
+        }
+  
+        // User is an agent, allow the request to proceed
+        next();
+      } catch (error) {
+        console.error('Error in checkIsAgent middleware:', error);
+        res.status(500).send({ 'message': 'Internal server error' });
       }
-
-      if (user.role !== 'AGENT') {
-        res.status(403).send({ 'message': 'User is not an agent' });
-        return;
-      }
-
-      // User is an agent, allow the request to proceed
-      next();
-    } catch (error) {
-      console.error('Error in checkIsAgent middleware:', error);
-      res.status(500).send({ 'message': 'Internal server error' });
+    } else{
+      res.status(401).send({'message': 'Missing token'});
     }
   }
 }
