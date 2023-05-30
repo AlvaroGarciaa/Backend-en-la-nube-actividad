72d15e700730f0dbdc774e60106e7d659008a9df
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2023-05-30 13:54:04 -0600

    Ya funcionan las rutas

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
+import db from '../models';
 
 class UserController extends AbstractController {
     protected validateBody(type: any) {
@@ -17,21 +18,76 @@ class UserController extends AbstractController {
         return this.instance;
     }
     protected initRoutes(): void {
-        this.router.get("/cuenta/deposito", this.postDeposit.bind(this));
-        this.router.get("/cuenta/retiro", this.postWithdraw.bind(this));
-        this.router.post("/cuenta/saldo", this.getBalance.bind(this));
+        this.router.post("/deposito",this.authMiddleware.verifyToken, this.postDeposit.bind(this));
+        this.router.post("/retiro",this.authMiddleware.verifyToken, this.postRetiro.bind(this));
+        this.router.post("/saldo",this.authMiddleware.verifyToken, this.getBalance.bind(this));
         //Todas las rutas que necesite su controlador
     }
-    private postDeposit(req: Request, res: Response) {
-        res.status(200).send("servicio de lectura de usuarios");
+
+    private async postDeposit(req: Request, res: Response) {
+        try {
+          const { email, montoDeposito } = req.body;
+            
+          const user1 = await db.User.findOne({
+            where: {
+              email:email
+            }
+          })
+          console.log(typeof user1.balance)
+          console.log(typeof montoDeposito)
+          const newBalance = parseInt(user1.balance) + montoDeposito;
+          console.log(newBalance)
+          // Consulta el saldo actual
+          const user = await db.User.update(
+            { balance: newBalance },
+            { where: { email: email } }
+          );
+            res.status(200).send({ mensaje: 'Depósito realizado exitosamente' });
+        } catch (error) {
+          res.status(500).send({ error: 'Error al realizar el depósito' });
+        }
     }
-    private postWithdraw(req: Request, res: Response) {
-        res.status(200).send("Alta usuario");
+    
+    private async postRetiro(req: Request, res: Response) {
+        try {
+            const { email, montoRetiro } = req.body;
+      
+            // Consulta el saldo actual
+            const user1 = await db.User.findOne({
+                where: {
+                  email:email
+                }
+              });
+              const newBalance = parseInt(user1.balance) - montoRetiro;
+              console.log(newBalance)
+            const user = db.User.update(
+              { balance: newBalance },
+              { where: { email: email } }
+            );
+              res.status(200).send({ mensaje: 'Retiro realizado exitosamente' });
+          } catch (error) {
+            res.status(500).send({ error: 'Error al realizar el retiro' });
+          }
     }
 
-    private getBalance(req: Request, res: Response) {
-        res.status(200).send("Alta usuario");
-    }
+    private async getBalance(req: Request, res: Response) {
+        try {
+          const {email} = req.body;
+      
+          // Consulta el saldo actual
+          const user = await db.User.findOne({
+            where: {
+              email:email
+            }
+          });
+          res.status(200).send(user.balance).end();
+    
+        } catch (error) {
+          res.status(500).send({ error: 'Error al consultar el saldo' });
+        }
+      }
+      
+
 }
 
 export default UserController;
